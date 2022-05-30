190c80cbf324c7b7df815b40c6e0002382183dee
Mir Sujat <mir.sujat@gmail.com>
2022-05-30 15:41:18 +0600

    mir

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Product from './Components/Product/Product';
 
 function App() {
   return (
     <div className="App">
       Hello React 
+      <Product></Product>
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const client = new QueryClient();
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
